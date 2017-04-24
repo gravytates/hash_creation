b2d74da2c2d7bb146ca444b494739c1c6879b943
Grady Shelton and Kai de Rochemont <kai@>
2017-04-24 16:07:00 -0700

    pass second test to evaluate if a given key exists

--- lib/my_hash.rb
+++ lib/my_hash.rb
@@ -1,21 +1,72 @@
 class MyHash
-  @@pairs = []
   define_method(:initialize) do |key = nil, value = nil|
-    @key = key
-    @value = value
-    self.myStore(@key, @value)
+    @pairs = []
+    unless key.==(nil) || value.==(nil)
+      @pairs.push([key, value])
+    end
   end
 
-  define_method(:myShow) do
-    @@pairs
+  define_method(:Show) do
+    @pairs
   end
 
-  define_method(:myStore) do |key, value|
-    @@pairs.push(key)
-    @@pairs.push(value)
+  define_method(:Length) do
+    self.Show.length
   end
 
-  define_method(:myFetch) do |key|
-    self.myShow[self.myShow.index(key).+(1)]
+  define_method(:Store) do |key, value|
+    @pairs.push([key, value])
   end
+
+ define_method(:Fetch) do |key|
+    result = "No key found"
+    search = key
+    pairs = self.Show
+    pairs.each do |pair|
+      if pair[0].==(search)
+        result = pair[1]
+      end
+    end
+    result
+  end
+
+  define_method(:HasKey) do |key|
+    result = false
+    search = key
+    pairs = self.Show
+    pairs.each do |pair|
+      if pair[0].==(search)
+        result = true
+      end
+    end
+    result
+  end
+
+  define_method(:HasValue) do |value|
+    result = false
+    search = value
+    pairs = self.Show
+    pairs.each do |pair|
+      if pair[1].==(search)
+        result = true
+      end
+    end
+    result
+  end
+
+  define_method(:Merge) do |hash|
+    hash.Show.each do |pair|
+      @pairs.push(pair)
+    end
+  end
+
 end
+
+test_hash = MyHash.new
+test_hash.Store("test_key", "test_value")
+test_hash.Store("other_key", "other_value")
+test_hash.Store("third_key", "third_value")
+other_hash = MyHash.new
+other_hash.Store("test_key", "test_value")
+other_hash.Store("other_key", "other_value")
+other_hash.Store("third_key", "third_value")
--- spec/my_hash_spec.rb
+++ spec/my_hash_spec.rb
@@ -2,11 +2,21 @@ require('rspec')
 require('my_hash.rb')
 
 describe(MyHash) do
-  describe("#myFetch") do
+
+  describe("#Fetch") do
     it("retrieves a stored value by its key") do
       test_hash = MyHash.new()
-      test_hash.myStore("kitten", "cute")
-      expect(test_hash.myFetch("kitten")).to(eq("cute"))
+      test_hash.Store("kitten", "cute")
+      expect(test_hash.Fetch("kitten")).to(eq("cute"))
     end
   end
+
+  describe("#HasKey") do
+    it("Checks if a given key exists within the hash") do
+      test_hash = MyHash.new()
+      test_hash.Store("kitten", "cute")
+      expect(test_hash.HasKey("kitten")).to(eq(true))
+    end
+  end
+
 end
